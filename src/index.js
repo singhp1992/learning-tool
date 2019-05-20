e2a05e3184925841b94606d0afb345cf309e376d
Preeti <singhp1992@gmail.com>
2019-05-20 17:24:53 +0200

    removing the register from SW

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import store from './store'
 
